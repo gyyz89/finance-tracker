8022212a78abdb841a421082e4ad8a4f2b45b520
Graeme Yap <gyyz89@gmail.com>
2018-04-20 12:24:59 +0000

    Complete finance tracker app

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: 'users#add_friend'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
